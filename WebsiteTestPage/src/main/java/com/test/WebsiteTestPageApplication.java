57a6b64a43bb54eb48c86a145ee7e0048d78db50
Warren Peterson <77131351+WarrenGSlice@users.noreply.github.com>
2024-01-18 13:41:11 -0800

    Update WebsiteTestPageApplication.java

--- WebsiteTestPage/src/main/java/com/test/WebsiteTestPageApplication.java
+++ WebsiteTestPage/src/main/java/com/test/WebsiteTestPageApplication.java
@@ -13,4 +13,41 @@ public class WebsiteTestPageApplication {
 		SpringApplication.run(WebsiteTestPageApplication.class, args);
 	}
 
+	/**
+	 * Allows Swagger-Ui to Operate without throwing an MVC error
+	 * @param webEndpointsSupplier - Web Endpoints Supplier
+	 * @param servletEndpointsSupplier - Servlet Endpoints Supplier
+	 * @param controllerEndpointsSupplier - Controller End Points Supplier
+	 * @param endpointMediaTypes - End Point Media Types
+	 * @param corsProperties - Cors Properties
+	 * @param webEndpointProperties - Web End Points Properties
+	 * @param environment - Environment
+	 * @return WebMvcEndpointHandlerMapping, EndpointLinksResolver
+	 */
+	@SuppressWarnings("unchecked")
+	@Bean
+	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
+		@SuppressWarnings("rawtypes")
+		List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+	    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+	    allEndpoints.addAll(webEndpoints);
+	    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+	    String basePath = webEndpointProperties.getBasePath();
+	    EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+	}
+
+	/**
+	 * Helper Method for WebMvcEndpointHandlerMapping
+	 * @param webEndpointProperties
+	 * @param environment
+	 * @param basePath
+	 * @return webEndpointProperties, StringUtils, ManagementPortType
+	 */
+	private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+	    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+	}
+
 }
